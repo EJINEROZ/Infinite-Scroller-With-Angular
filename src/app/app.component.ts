51bf3ffabab0eed447f772a2a658910c5e5f0bca
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-07-25 00:54:10 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Comment } from './comment.model';
+import { CommentService } from './comment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sroller';
+export class AppComponent implements OnInit {
+  throttle = 0;
+  distance = 2;
+  page = 1;
+  commentaries: Comment[] | any[] = [];
+
+  constructor(private commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this.commentService
+      .getCommentaries(this.page)
+      .subscribe((commentaries: Comment[]) => {
+        this.commentaries = commentaries;
+      });
+  }
+
+  onScroll(): void {
+    this.commentService
+      .getCommentaries(++this.page)
+      .subscribe((commentaries: Comment[]) => {
+        this.commentaries.push(...commentaries);
+      });
+  }
 }
